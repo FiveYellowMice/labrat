1c9c8b7e1f59b107b78cbacc979bc8eccd6f102d
Kaizhe Huang <hkz85825915@gmail.com>
2016-10-28 17:14:02 +1100

    Change tab to 2 spaces

--- labrat.js
+++ labrat.js
@@ -6,23 +6,23 @@ const twitterSync = require("./lib/twitter.js");
 
 console.log("Starting LabRat...");
 http.createServer(function(request, response) {
-	var webhookRegex = /\/webhooks\/(.*?)\/.*/;
-	if (webhookRegex.test(request.url)) {
-		switch (request.url.match(webhookRegex)[1]) {
-			case "telegram":
-				telegramBot.processWebhook(request, response);
-			default:
-				response.statusCode = 404;
-				response.end("No such webhook.\n");
-		}
-		return;
-	}
+  var webhookRegex = /\/webhooks\/(.*?)\/.*/;
+  if (webhookRegex.test(request.url)) {
+    switch (request.url.match(webhookRegex)[1]) {
+      case "telegram":
+        telegramBot.processWebhook(request, response);
+      default:
+        response.statusCode = 404;
+        response.end("No such webhook.\n");
+    }
+    return;
+  }
 
-	// Currently no other HTTP services needed
-	response.statusCode = 404;
-	response.end("Not found.\n");
+  // Currently no other HTTP services needed
+  response.statusCode = 404;
+  response.end("Not found.\n");
 }).listen(config.listenPort, config.listenAddress, () => {
-	console.log(`HTTP server started, listening on [${config.listenAddress}]:${config.listenPort}.`);
+  console.log(`HTTP server started, listening on [${config.listenAddress}]:${config.listenPort}.`);
 });
 
 config.rss.url && rssReceiver.start();
